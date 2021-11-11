1ed0be0100b300886707b94da888361eaaf16483
ThanmayaAI3 <65693905+ThanmayaAI3@users.noreply.github.com>
2021-11-10 19:28:13 -0500

    Delete Post.java

--- Post.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Post {
-    private User owner;
-    private String content;
-    private String topic;
-    private ArrayList<Comment> comments = new ArrayList<Comment>();
-
-}
