555206fae8edb8ed871ff6e8965bec189237c9c3
Your Name <you@example.com>
2021-11-10 19:53:36 -0500

    assign roles

--- src/ProgramManager.java
+++ src/ProgramManager.java
@@ -4,35 +4,35 @@ public class ProgramManager {
     private ArrayList<User> users = new ArrayList<>();
     private ArrayList<Post> posts = new ArrayList<>();
 
-    public void writeFile(){
+    public void writeFile(){// aidan
         //write from arraylist to txt files
         //updates the txt files
     }
-    public void readFile(){
+    public void readFile(){ // aidan
         //parse through the lines in the file to the Array list
         //updates the arraylist
     }
-    public void addPost(Post post){
+    public void addPost(Post post){ // Thanmaya
         //adds a post
         //updates arraylist and file
     }
-    public void removePost(Post post){
+    public void removePost(Post post){// Thanmaya
         //removes from the array list
         //updates arraylist and file
     }
-    public void modifyPost(Post post){
+    public void modifyPost(Post post){// Thanmaya
         //modifies post
         //updates arraylist and file
     }
-    public void addUser(User user){
+    public void addUser(User user){ // Thanmaya
         //adds a user
         //updates arraylist and file
     }
-    public void removeUser(User user){
+    public void removeUser(User user){ // Thanmaya
         //removes from array list
         //updates arraylist and file
     }
-    public void modifyUser(User user){
+    public void modifyUser(User user){ // Thanmaya
         //modifies user
         //updates arraylist and file
     }
