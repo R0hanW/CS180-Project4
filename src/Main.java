358264b040c0fb4b092e07dd6b331cc58800ddf7
ThanmayaAI3 <65693905+ThanmayaAI3@users.noreply.github.com>
2021-11-10 19:20:18 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        //all the menus go here (asking to login/modify/log out etc.)
+    }
+}
---
+++ src/Post.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Post {
+    private User owner;
+    private String content;
+    private String topic;
+    private ArrayList<Comment> comments = new ArrayList<Comment>();
+
+}
