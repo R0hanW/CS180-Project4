1d50490413488bd83a55d7d6f6e898fa239f875e
Your Name <you@example.com>
2021-11-10 19:49:55 -0500

    testing commits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         //all the menus go here (asking to login/modify/log out etc.)
+        System.out.println("testing commit");
     }
 }
