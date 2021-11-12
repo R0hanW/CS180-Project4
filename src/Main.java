778eafbe67073e58b073b5ab8e35f9ef016ba491
Arj2021 <54119571+Arj2021@users.noreply.github.com>
2021-11-11 22:40:28 -0500

    Main update
    Basic login/signup

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         String explanation = "Type the number next to the option you would like to choose";
+        String yesNo = "[1]Yes\n[2]No";
+        int signUpResponse = 2;
         //all the menus go here (asking to login/modify/log out etc.)
         Scanner scan = new Scanner(System.in);
         ProgramManager program = new ProgramManager();
@@ -14,22 +16,55 @@ public class Main {
         System.out.println("[2]Sign Up");
         int input = scan.nextInt();
         scan.nextLine();
+
+        //Log in page
         if(input == 1){
             //System.out.println(explanation);
-            System.out.print("Enter the Username: ");
+            System.out.println("Enter the Username");
             String username = scan.nextLine();
-            System.out.print("\nEnter the Password: ");
-            String password = scan.nextLine();
+            System.out.println("Enter the Password");
+            String password;
+            password = scan.nextLine();
             User user = program.findUser(username);
-            if(user.getPassword().equals(password)){
-
+            if(user != null) {
+                while(!(user.getPassword().equals(password))) {
+                    System.out.println("Wrong Password, Please Re-enter");
+                    password = scan.nextLine();
+                }
+                System.out.println("Logged In Successfully!");
+            }
+            else{
+                System.out.println("Username doesn't exist, would you like to sign up?");
+                System.out.println(explanation);
+                System.out.println(yesNo);
+                signUpResponse = scan.nextInt();
+                scan.nextLine();
             }
         }
 
-
-
-
-        //System.out.println("testing commit");
+        //sign up page
+        if(signUpResponse == 1 || input == 2){
+            System.out.println("What is your name");
+            String newName = scan.nextLine();
+            System.out.println("Enter the name you would like to keep for your username");
+            String newUsername = scan.nextLine();
+            System.out.println("Enter what would you like to be for your password");
+            String newPassword = scan.nextLine();
+            System.out.println("Are you a teacher?");
+            System.out.println(explanation);
+            System.out.println(yesNo);
+            boolean isTeacher = false;
+            int ans = scan.nextInt();
+            scan.nextLine();
+            if(ans == 1){
+                isTeacher = true;
+            }
+            else if(ans == 2){
+                isTeacher = false;
+            }
+            User newUser = new User(newName,newUsername,newPassword,isTeacher);
+            program.addUser(newUser);
+        }
 
 
     }
