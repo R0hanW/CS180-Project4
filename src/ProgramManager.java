743ea01a7d12fb4126152860b21119396ef2e732
Arj2021 <54119571+Arj2021@users.noreply.github.com>
2021-11-14 22:43:10 -0500

    main for commit

--- src/ProgramManager.java
+++ src/ProgramManager.java
@@ -31,7 +31,7 @@ public class ProgramManager {
     public void writeFile() throws Exception{// works
         //write from arraylist to txt files
         //updates the txt files
-    	//if writing to the user storage file
+        //if writing to the user storage file
         File f = new File("src/Users.txt");
         PrintWriter pw = new PrintWriter(new FileWriter(f, false));
         for (int i = 0; i < users.size(); i++) {	//loop to print a user to each line in file
@@ -42,19 +42,19 @@ public class ProgramManager {
         pw = new PrintWriter(new FileWriter(new File("src/Courses.txt"), false));
         for(Course course: courses) pw.println(course.toString());
         pw.close();
-    	// } else {	//if writing to post storage file
-    	// 	File f = new File("Posts.txt");
-    	// 	try (PrintWriter pw = new PrintWriter(new FileWriter(f, false))) {
+        // } else {	//if writing to post storage file
+        // 	File f = new File("Posts.txt");
+        // 	try (PrintWriter pw = new PrintWriter(new FileWriter(f, false))) {
         //     	for (int i = 0; i < users.size(); i++) {	//loop to print a post to each line in file
         //     		pw.println(posts.get(i).toString());
         //     	}
         //     } catch (IOException e) {	//exception handling
         //     	e.printStackTrace();
         //     }
-    	// }
+        // }
     }
     public void readFile() throws Exception{
-         //reads from Courses.txt (won't work unless users has already been read)
+        //reads from Courses.txt (won't work unless users has already been read)
         BufferedReader reader = new BufferedReader(new FileReader("src/Courses.txt"));
         String message;
         Course course = null;
@@ -106,85 +106,85 @@ public class ProgramManager {
     public void readUserFile(){ // works
         //parse through the lines in the file to the Array list
         //updates the arraylist
-    	File f = new File("src/Users.txt");
+        File f = new File("src/Users.txt");
         try (BufferedReader bfr = new BufferedReader(new FileReader(f))) {
-        	String line = bfr.readLine();
-        	if (line == null) {
-        		return;
-        	}
-        	while (line != null) {
-        		int numQuotes = 0;
-        		int[] quoteIndices = new int[8];	//each line should have 8 quotation marks
-        											//one pair for each field of a user
-        		for(int i = 0; i < line.length(); i++) {
-        			if (line.charAt(i) == '"') {
-        				numQuotes++;
-        				if (numQuotes < 9) {
-            				quoteIndices[numQuotes - 1] = i;        					
-        				}
-        			}
-        		}
-        		
-        		if (numQuotes != 8) {	//checking for storage format error
-        			System.out.println("Internal storage error. Your account may have been deleted.");
-        	        try (PrintWriter pw = new PrintWriter(new FileWriter(f, false))) {
-        	        	for (int i = 0; i < users.size(); i++) {
-        	        		pw.println(users.get(i).toString());	//salvages user info before error
-        	        	}
-        	        } catch (IOException e) {	//exception handling
-        	        	e.printStackTrace();
-        	        }
-        			return;
-        		}
-        		//adds new user to list of users
-        		users.add(new User(line.substring(quoteIndices[0] + 1, quoteIndices[1]),
-        				line.substring(quoteIndices[2] + 1, quoteIndices[3]), 
-        				line.substring(quoteIndices[4] + 1, quoteIndices[5]),
-        				(line.substring(quoteIndices[6] + 1, quoteIndices[7]).toLowerCase().equals("true"))));
-        		line = bfr.readLine();
-        	}
+            String line = bfr.readLine();
+            if (line == null) {
+                return;
+            }
+            while (line != null) {
+                int numQuotes = 0;
+                int[] quoteIndices = new int[8];	//each line should have 8 quotation marks
+                //one pair for each field of a user
+                for(int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == '"') {
+                        numQuotes++;
+                        if (numQuotes < 9) {
+                            quoteIndices[numQuotes - 1] = i;
+                        }
+                    }
+                }
+
+                if (numQuotes != 8) {	//checking for storage format error
+                    System.out.println("Internal storage error. Your account may have been deleted.");
+                    try (PrintWriter pw = new PrintWriter(new FileWriter(f, false))) {
+                        for (int i = 0; i < users.size(); i++) {
+                            pw.println(users.get(i).toString());	//salvages user info before error
+                        }
+                    } catch (IOException e) {	//exception handling
+                        e.printStackTrace();
+                    }
+                    return;
+                }
+                //adds new user to list of users
+                users.add(new User(line.substring(quoteIndices[0] + 1, quoteIndices[1]),
+                        line.substring(quoteIndices[2] + 1, quoteIndices[3]),
+                        line.substring(quoteIndices[4] + 1, quoteIndices[5]),
+                        (line.substring(quoteIndices[6] + 1, quoteIndices[7]).toLowerCase().equals("true"))));
+                line = bfr.readLine();
+            }
         } catch (IOException e) { //exception handling
-        	e.printStackTrace();
+            e.printStackTrace();
         }
     }
     public String readUserFileImport(String filename) throws FileNotFoundException {
-    	String text = "";
-    	File f = new File(filename);
-    	try (BufferedReader bfr = new BufferedReader(new FileReader(f))) {
-    		String line = bfr.readLine();
-    		while (line != null) {
-    			text += line;
-    			text = bfr.readLine();
-    		}
-    	} catch (FileNotFoundException e) {
-    		throw e;
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    	return text;
+        String text = "";
+        File f = new File(filename);
+        try (BufferedReader bfr = new BufferedReader(new FileReader(f))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                text += line;
+                text = bfr.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
     }
     public void addCourse(Course course){
         courses.add(course);
     }
     public void removeCourse(Course course){
         courses.remove(course);
-    }   
+    }
     public void modifyCourse(Course oldCourse, String name){
         oldCourse.setName(name);
 
     }
     public void addUser(User user){ // works
-		users.add(user);
+        users.add(user);
     }
     public boolean removeUser(User user){ // works
         user.removeComments();
-		users.remove(user);
-		return false; //if user asked to remove doesnt exist
+        users.remove(user);
+        return false; //if user asked to remove doesnt exist
     }
     public void modifyUser(User oldUser, String name, String username, String password, boolean isTeacher){ // works
-		oldUser.setName(name);
-		oldUser.setUsername(username);
-		oldUser.setPassword(password);
+        oldUser.setName(name);
+        oldUser.setUsername(username);
+        oldUser.setPassword(password);
         oldUser.setIsTeacher(isTeacher);// an object from the array has to be passed (or another reference object in the array)
     }
 
@@ -197,7 +197,7 @@ public class ProgramManager {
         return null;
         //return users.stream().filter(user -> user.getUsername() == username).findAny().orElse(null);
     }
-    
+
     // //returns list of comments sorted by votes in descending order
     // public ArrayList<Comment> sortByVotes(int courseNumber) {
     //     ArrayList<Comment> commentList = new ArrayList<Comment>();
